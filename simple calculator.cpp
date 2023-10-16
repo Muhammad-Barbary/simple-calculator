ffbdb98848042412963b63855a5aafaa5e3af229
Muhammad-Barbary <135440535+Muhammad-Barbary@users.noreply.github.com>
2023-10-16 21:04:13 +0300

    Update simple calculator.cpp

--- simple calculator.cpp
+++ simple calculator.cpp
@@ -20,7 +20,7 @@ int main(){
 	cout << "[2] subtract" << endl;
 	cout << "[3] multiply" << endl;
 	cout << "[4] divide" << endl;
-    cout << "[0 or any random bullshit] exit calculator" << endl;
+    cout << "[0] exit calculator" << endl;
 	cout << "\n=================\n" << endl;
 	cout << "select operation type ===> ";
 	cin >> opType;
